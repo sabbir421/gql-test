05c718c0694bfdca44ee39e67940347b7e2689a6
Sabbircoppanet <sabbir.a@coppanet.com>
2024-10-14 16:36:10 +0600

    refactor schema

--- typedefs/schema.js
+++ typedefs/schema.js
@@ -1,63 +1,91 @@
-const { gql } = require('apollo-server');
-const { GraphQLJSON } = require('graphql-type-json');
+const { gql } = require("apollo-server");
+const { GraphQLJSON } = require("graphql-type-json");
 
 const typeDefs = gql`
   scalar Long
   scalar JSON
 
-  type Position {
-    x: Float
-    y: Float
-  }
-
-  type NodeObject {
-    _id: ID!
-    name: String!
-    description: String
-    trigger: [Trigger]
-    preActions: JSON
-    responses: [Response]
-    postActions: JSON
-    actions: [Action]  # Updated actions field
-    compositeId: ID
-    global: Boolean
-    redirect: JSON
-    analytics: JSON
-    memberTagging: JSON
-    type: String
-    tags: JSON
-    saveCompositeId: Boolean
-    createdAt: Long!
-    updatedAt: Long
-    parents: [NodeObject]
-    root: Boolean
-    position: Position
-  }
-
   type Action {
-    _id: ID!  # Updated to use _id
-    name: String
-    description: String
-    nodeId: ID  # Assuming each action is associated with a nodeId
-    createdAt: Long
-  }
-
-  type Response {
     _id: ID!
     name: String
     description: String
-    platforms: JSON
-    tags: [String]
     createdAt: Long
+    updatedAt: Long
+    functionString: String
+    resourceTemplateId: ID
+    resourceTemplate: ResourceTemplate
   }
 
   type Trigger {
     _id: ID!
-    # Add other relevant fields for Trigger here
+    name: String!
+    description: String
+    createdAt: Long
+    updatedAt: Long
+    functionString: String
+    resourceTemplateId: ID
+    resourceTemplate: ResourceTemplate
   }
 
+  type Response {
+    _id: ID!
+    name: String!
+    description: String
+    createdAt: Long
+    updatedAt: Long
+    platforms: [ResponsePlatform]
+  }
+
+  type ResponsePlatform {
+    integrationId: ID
+    build: Int
+    localeGroups: [ResponseLocaleGroup]
+  }
+
+  type ResponseLocaleGroup {
+    localeGroupId: ID
+    variations: [ResponseVariation]
+  }
+
+  type ResponseVariation {
+    name: String!
+    responses: JSON
+  }
+
+  type ResourceTemplate {
+    _id: ID!
+    name: String!
+    description: String
+    schema: JSON
+    integrationId: String
+    functionString: String
+    key: String
+    createdAt: Long
+    updatedAt: Long
+  }
+
+type NodeObject {
+  _id: ID!
+  name: String!
+  description: String
+  createdAt: Long!
+  updatedAt: Long
+  parents: [NodeObject]
+  parentIds: [ID]
+  root: Boolean
+  triggerId: ID
+  trigger: [Trigger]
+  responseIds: [ID]
+  actions: [Action]
+  responses: [Response]
+  priority: Float
+  compositeId: ID
+  global: Boolean
+  colour: String
+}
+
   type Query {
-    node(nodeId: ID): NodeObject
+    node(nodeId: ID!): NodeObject
   }
 `;
 
