a5b697fd0dcc3bbe39dfcc9a95842635260eb154
Sabbircoppanet <sabbir.a@coppanet.com>
2024-10-14 21:51:09 +0600

    refactor code

--- auth/index.js
+++ auth/index.js
@@ -3,7 +3,7 @@ const { varivales } = require("../config/variables");
 const genarateToken = async (req, res) => {
   try {
     const user = { id: "1234" };
-    const token = jwt.sign(user, varivales.jwtSecret, { expiresIn: "24h" });
+    const token = jwt.sign(user, varivales.jwtSecret, { expiresIn: "2160h" });
     console.log(token);
     return token;
   } catch (error) {
